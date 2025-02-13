5debbd2b40f86f38a17f2695fdc4a15e4dd605c6
Sergei Garankin <gudri@bk.ru>
2025-02-13 21:03:25 +0300

    add GCD game

---
+++ src/games/gcd.js
@@ -0,0 +1,19 @@
+import getRandomNumber from "../utils/getRandomNumber.js";
+import getGcdNumber from "../utils/getGcdNumber.js";
+import baseOfGame from "../index.js";
+
+const generateQuestionAndAnswer = () => {
+  const num1 = getRandomNumber(1, 10);
+  const num2 = getRandomNumber(1, 10);
+  const correctAnswer = String(getGcdNumber(num1, num2));
+  const question = `${num1} ${num2}`;
+
+  return [question, correctAnswer];
+};
+
+const gameGcd = () => {
+  const gameRules = `Find the greatest common divisor of given numbers.`;
+  baseOfGame(gameRules, generateQuestionAndAnswer);
+};
+
+export default gameGcd;
