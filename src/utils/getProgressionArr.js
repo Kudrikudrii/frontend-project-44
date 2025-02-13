d000091e8bfac91fe4ae0a10c00f197be8fff068
Sergei Garankin <gudri@bk.ru>
2025-02-14 02:01:50 +0300

    add src/utils/getProgressionArr.js

---
+++ src/utils/getProgressionArr.js
@@ -0,0 +1,14 @@
+import getRandomNumber from ".//getRandomNumber.js";
+
+const getProgressionArr = () => {
+  const startNumber = getRandomNumber(5, 20);
+  const progressionStep = getRandomNumber(2, 4);
+  const arrLength = getRandomNumber(5, 10);
+  const progressionArr = [];
+
+  for (let i = 0; i < arrLength; i += 1) {
+    progressionArr.push(startNumber + i * progressionStep);
+  }
+  return progressionArr;
+};
+export default getProgressionArr;
