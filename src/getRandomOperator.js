0acfd76435c2c5482ed94020afae9a28feb2a192
Sergei Garankin <gudri@bk.ru>
2025-02-13 15:40:17 +0300

    move to utils/

--- src/getRandomOperator.js
+++
@@ -1,8 +0,0 @@
-const getRandomOperator = () => {
-  const arrayOperators = ["+", "-", "*"];
-  const operatorIndex = Math.floor(Math.random() * arrayOperators.length);
-  const operator = arrayOperators[operatorIndex];
-  return operator;
-};
-
-export default getRandomOperator;
