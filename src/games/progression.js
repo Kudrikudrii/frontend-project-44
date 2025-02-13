36ca32a032103784c3ba85e664ca36bf13f5cf4d
Sergei Garankin <gudri@bk.ru>
2025-02-14 02:03:18 +0300

    add progression game

---
+++ src/games/progression.js
@@ -0,0 +1,22 @@
+import getRandomNumber from "../utils/getRandomNumber.js";
+import getProgressionArr from "../utils/getProgressionArr.js";
+import baseOfGame from "../index.js";
+
+const generateQuestionAndAnswer = () => {
+  //   const secretNumber = "..";
+  const progression = [...getProgressionArr()];
+  const randomIndex = getRandomNumber(0, progression.length - 1);
+  const correctAnswer = progression[randomIndex];
+  progression[randomIndex] = "..";
+
+  const question = progression.join(" ");
+
+  return [question, String(correctAnswer)];
+};
+
+const gameProgression = () => {
+  const gameRules = `What number is missing in the progression?`;
+  baseOfGame(gameRules, generateQuestionAndAnswer);
+};
+
+export default gameProgression;
