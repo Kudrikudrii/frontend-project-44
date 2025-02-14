a41f7bacea40966b79c864f71c32da7ff1ed2cdd
Сергей <Sergei@MacBook-Air-Sergej.local>
2025-02-14 20:25:01 +0300

    add src/utils/isPrime.js

---
+++ src/utils/isPrime.js
@@ -0,0 +1,15 @@
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export default isPrime;
