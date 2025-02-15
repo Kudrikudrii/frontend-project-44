fd5372033bce069a49dce6d4cb9992ffb8394f3c
Sergei <gudri@bk.ru>
2025-02-15 14:32:49 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,17 +1,17 @@
-import getRandomNumber from "../utils/getRandomNumber.js";
-import baseOfGame from "../index.js";
+import getRandomNumber from '../utils/getRandomNumber.js';
+import baseOfGame from '../index.js';
 
 const generateQuestionAndAnswer = () => {
   const number = getRandomNumber(1, 10);
   const question = `${number}`;
   const isEven = number % 2 === 0;
-  const correctAnswer = isEven ? "yes" : "no";
+  const correctAnswer = isEven ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
 
 const gameEvenOrOdd = () => {
-  const gameRules = `Answer "yes" if the number is even, otherwise answer "no".`;
+  const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
   baseOfGame(gameRules, generateQuestionAndAnswer);
 };
 
