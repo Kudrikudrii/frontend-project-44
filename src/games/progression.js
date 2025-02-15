0453e3f8d626e3fd968b3c03740996a6f184b301
Sergei <gudri@bk.ru>
2025-02-15 14:39:47 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,20 @@
-import getRandomNumber from "../utils/getRandomNumber.js";
-import getProgressionArr from "../utils/getProgressionArr.js";
-import baseOfGame from "../index.js";
+import getRandomNumber from '../utils/getRandomNumber.js';
+import getProgressionArr from '../utils/getProgressionArr.js';
+import baseOfGame from '../index.js';
 
 const generateQuestionAndAnswer = () => {
-  //   const secretNumber = "..";
   const progression = [...getProgressionArr()];
   const randomIndex = getRandomNumber(0, progression.length - 1);
   const correctAnswer = progression[randomIndex];
-  progression[randomIndex] = "..";
+  progression[randomIndex] = '..';
 
-  const question = progression.join(" ");
+  const question = progression.join(' ');
 
   return [question, String(correctAnswer)];
 };
 
 const gameProgression = () => {
-  const gameRules = `What number is missing in the progression?`;
+  const gameRules = 'What number is missing in the progression?';
   baseOfGame(gameRules, generateQuestionAndAnswer);
 };
 
