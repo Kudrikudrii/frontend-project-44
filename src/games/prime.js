e3c6c32f6759fd96d3d03e1518894ef3599016ef
Sergei <gudri@bk.ru>
2025-02-15 14:37:45 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,17 +1,17 @@
-import getRandomNumber from "../utils/getRandomNumber.js";
-import isPrime from "../utils/isPrime.js";
-import baseOfGame from "../index.js";
+import getRandomNumber from '../utils/getRandomNumber.js';
+import isPrime from '../utils/isPrime.js';
+import baseOfGame from '../index.js';
 
 const generateQuestionAndAnswer = () => {
   const number = getRandomNumber(1, 15);
   const question = `${number}`;
   const isNumberPrime = isPrime(number);
-  const correctAnswer = isNumberPrime ? "yes" : "no";
+  const correctAnswer = isNumberPrime ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
 const gamePrime = () => {
-  const gameRules = `Answer "yes" if given number is prime. Otherwise answer "no".`;
+  const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   baseOfGame(gameRules, generateQuestionAndAnswer);
 };
 
